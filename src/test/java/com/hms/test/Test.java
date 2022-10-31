8f4ba6fde12665a7b7a69f55083ef283d2a87911
huyidao <huyidao00@gmail.com>
2022-10-31 10:36:21 +0800

    commit

--- src/test/java/com/hms/test/Test.java
+++ src/test/java/com/hms/test/Test.java
@@ -3,29 +3,19 @@ package com.hms.test;
 
 import com.hms.util.FileOperations;
 
-import java.io.File;
-import java.util.ArrayList;
-
 public class Test {
     public static void main(String[] args) {
         FileOperations fo = new FileOperations();
-//        fo.copy("src", "1");
-//        fo.cut("1*", "F:\\HMS\\Desktop\\a");
+
+       fo.copy("src", "1");
+       fo.cut("1", "F:\\HMS\\Desktop\\a");
 //        fo.deleteFile("F:\\HMS\\Desktop\\test\\WorkorderController.java");
 //        fo.searchByFileName("E:/", ".java");
 //
 //        fo.appendFile("F:\\HMS\\Desktop\\hello.txt", "hello");
 //
-        ArrayList<File> arrayList = fo.getAllFileInDirectory("F:\\HMS\\Desktop");
-        for (File file : arrayList) {
-            System.out.println(file.getAbsolutePath());
-        }
 
-        ArrayList<File> arrayList1 = fo.getAllFileInDirectory("F:\\HMS\\Desktop");
-        for (File file : arrayList) {
-            System.out.println(file.getAbsolutePath());
-        }
-
+        //fo.deleteFile("1");
 //        File file=new File("E:\\file.doc");
 //        String fileName=file.getName();
 //        String fileTyle=fileName.substring(fileName.lastIndexOf(".")+1);
--- src/test/java/com/hms/util/FileOperations.java
+++ src/test/java/com/hms/util/FileOperations.java
@@ -539,6 +539,6 @@ public class FileOperations {
         }
         return true;
     }
-
+    //
 
 }
