615922b46a5a2716db094e87a02f211660754f54
huyidao <huyidao00@gmail.com>
2022-11-02 10:06:02 +0800

    commit

--- src/test/java/com/hms/test/Test.java
+++ src/test/java/com/hms/test/Test.java
@@ -2,12 +2,10 @@ package com.hms.test;
 
 
 public class Test {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         long startTime = System.nanoTime();   //获取开始时间
 
         System.out.println("----------------------------------------------开始----------------------------------------------");
-
-
 
 
         System.out.println("----------------------------------------------结束----------------------------------------------");
---
+++ src/test/java/com/hms/util/CompileJava.java
@@ -0,0 +1,32 @@
+package com.hms.util;
+
+import java.io.IOException;
+
+/**
+ * java编译
+ */
+public class CompileJava {
+
+    /**
+     * 编译Java文件
+     *
+     * @param encoding     源文件编码
+     * @param classesPath  目标文件存放目录
+     * @param jarPath      引用的jar包目录
+     * @param javaFilePath java源文件位置
+     */
+    public static void compileJavaFile(String encoding, String classesPath, String jarPath, String javaFilePath) {
+        try {
+            // javac -encoding [encoding]  -d [classesPath] -Djava.ext.dirs=[jarPath]  [javaFilePath]
+            Runtime.getRuntime().exec("cmd.exe /C javac "
+                    + "-encoding " + encoding
+                    + " -d " + classesPath
+                    + " -Djava.ext.dirs=" + jarPath
+                    + " " + javaFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
