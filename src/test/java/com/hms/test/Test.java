ff094f092b9dd5c89f8dc02952062c078bafb7b2
huyidao <huyidao00@gmail.com>
2022-11-02 17:06:50 +0800

    commit

--- src/test/java/com/hms/test/Test.java
+++ src/test/java/com/hms/test/Test.java
@@ -1,11 +1,21 @@
 package com.hms.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Test {
-    public static void main(String[] args) {
-        long startTime = System.nanoTime();   //获取开始时间
 
+    public static void main(String[] args) throws InterruptedException {
+        long startTime = System.nanoTime();   //获取开始时间
         System.out.println("----------------------------------------------开始----------------------------------------------");
+
+        Logger logger = LoggerFactory.getLogger("LoggerName（记录器名称）");
+
+        logger.trace("test_trace");
+        logger.debug("test_debug");
+        logger.info("test_info");
+        logger.warn("test_warn");
+        logger.error("test_error");
 
 
         System.out.println("----------------------------------------------结束----------------------------------------------");
